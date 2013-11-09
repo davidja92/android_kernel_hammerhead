aeb88ef76be437aa7dcdbe5ce99348e7dde1f9e5
n3ocort3x <peter.hofinger@gmail.com>
2013-11-09 02:04:12 +0100

    earlysuspend: Sorry Google, but we want that ;)

--- kernel/power/earlysuspend.c
+++ kernel/power/earlysuspend.c
@@ -17,7 +17,6 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/rtc.h>
-#include <linux/syscalls.h> /* sys_sync */
 #include <linux/wakelock.h>
 #include <linux/workqueue.h>
 
@@ -28,7 +27,14 @@ enum {
 	DEBUG_SUSPEND = 1U << 2,
 	DEBUG_VERBOSE = 1U << 3,
 };
+
+#ifdef CONFIG_PM_DEBUG
+/* debug callbacks registered with early_suspend and late_resume rotuines */
+static int debug_mask = DEBUG_USER_STATE | DEBUG_SUSPEND;
+#else
 static int debug_mask = DEBUG_USER_STATE;
+#endif
+
 module_param_named(debug_mask, debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP);
 
 static DEFINE_MUTEX(early_suspend_lock);
@@ -103,10 +109,7 @@ static void early_suspend(struct work_struct *work)
 	}
 	mutex_unlock(&early_suspend_lock);
 
-	if (debug_mask & DEBUG_SUSPEND)
-		pr_info("early_suspend: sync\n");
-
-	sys_sync();
+	suspend_sys_sync_queue();
 abort:
 	spin_lock_irqsave(&state_lock, irqflags);
 	if (state == SUSPEND_REQUESTED_AND_SUSPENDED)
